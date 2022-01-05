fefb28e95f5c5a8f20f5cfd79e34688c4b94bab8
deepan <deepangrad@gmail.com>
2022-01-06 00:23:13 +0100

    updated anchors

--- object_detection/backbones/utils.py
+++ object_detection/backbones/utils.py
@@ -123,23 +123,24 @@ def get_prior_box_configuration(configuration_name='VOC'):
 
     elif configuration_name in {'SSD-ResNet20'}:
         configuration = {
-            'feature_map_sizes': [48, 24, 12, 6, 4, 2],
+            'feature_map_sizes': [48, 24, 12, 6, 4, 1],
             'image_size': 96,
-            'steps': [4, 8, 16, 32, 64, 96],
-            'min_sizes': [12, 21, 44, 70, 81, 90],
-            'max_sizes': [21, 44, 70, 81, 90, 103],
+            'steps': [2, 5, 10, 21, 31, 96],
+            'min_sizes': [8, 19, 35, 51, 68, 84],
+            'max_sizes': [19, 35, 51, 68, 84, 101],
             'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
             'variance': [0.1, 0.2]}
 
     elif configuration_name in {'SSD-MobileNet'}:
         configuration = {
-            'feature_map_sizes': [48, 24, 12, 6, 4, 2],
+            'feature_map_sizes': [48, 24, 12, 6, 4, 1],
             'image_size': 96,
-            'steps': [4, 8, 16, 32, 64, 96],
-            'min_sizes': [12, 21, 44, 70, 81, 90],
-            'max_sizes': [21, 44, 70, 81, 90, 103],
+            'steps': [2, 5, 10, 21, 31, 96],
+            'min_sizes': [8, 19, 35, 51, 68, 84],
+            'max_sizes': [19, 35, 51, 68, 84, 101],
             'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
             'variance': [0.1, 0.2]}
+
     else:
         raise ValueError('Invalid configuration name:', configuration_name)
     return configuration
